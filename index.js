1be4b8db992c7f11037842e03bf1330125b3c692
rajibulislam60 <rajibulislam60707877@gmail.com>
2024-11-23 21:12:34 +0600

    create post add start

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const express = require("express");
-const DBconnection = require("./config/dbconfig");
-const todoSchema = require("./model/todoSchema");
+const express = require("express")
+const DBconnection = require("./config/dbconfig")
+const todoSchema = require("./model/todoSchema")
 
-const app = express();
+const app = express()
+app.use(express.json())
 
-DBconnection();
-app.post("/", async (req, res) => {
-  let todo = new todoSchema({
-    name: "Rajibul",
-  });
-  let result = await todo.save();
-
-  res.status(201).send(result);
-});
+DBconnection()
+app.post("/createtask", async (req, res) => {
+  let { task } = req.body
+  let createtodo = new todoSchema({
+    task,
+  })
+  await createtodo.save();
+  res.status(201).send({ success: "created" })
+})
 
 app.listen(3000, () => {
-  console.log("server is running");
-});
+  console.log("server is running")
+})
--- model/todoSchema.js
+++ model/todoSchema.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 let todoSchema = new Schema({
-  name: String,
+  task: {
+    type: String,
+  },
 });
 
 module.exports = mongoose.model("Todo", todoSchema);
