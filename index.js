b8426d26e608e2fc767474ff8360876610920c37
rajibulislam60 <rajibulislam60707877@gmail.com>
2024-11-25 20:22:36 +0600

    database create read update and delete work test done

--- index.js
+++ index.js
@@ -1,20 +1,55 @@
-const express = require("express")
-const DBconnection = require("./config/dbconfig")
-const todoSchema = require("./model/todoSchema")
+const express = require("express");
+const DBconnection = require("./config/dbconfig");
+const todoSchema = require("./model/todoSchema");
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-DBconnection()
+DBconnection();
+
+// Database Create---------------------------------
 app.post("/createtask", async (req, res) => {
-  let { task } = req.body
+  let { task } = req.body;
   let createtodo = new todoSchema({
     task,
-  })
+  });
   await createtodo.save();
-  res.status(201).send({ success: "created" })
-})
+  res.status(201).send({ success: "created", data: createtodo });
+});
+
+// Database Read-------------------------------------
+app.get("/alltodo", async (req, res) => {
+  let alltodo = await todoSchema.find({});
+  res.status(200).send({ success: "data fetch successful", data: alltodo });
+});
+
+app.get("/singletodo/:id", async (req, res) => {
+  let { id } = req.params;
+  let singletodo = await todoSchema.findOne({ _id: id });
+  res
+    .status(200)
+    ._construct({ success: "single fetch successful", data: singletodo });
+});
+
+// Database Delete-----------------------------------------------
+app.delete("/deletetodo/:id", async (req, res) => {
+  let { id } = req.params;
+  let deletetodo = await todoSchema.findOneAndDelete({ _id: id });
+  res.status(200).send({ success: "todo deleted", data: deletetodo });
+});
+
+// Database Update------------------------------------------------
+app.patch("/updatetodo/:id", async (req, res) => {
+  let { id } = req.params;
+  let { task } = req.body;
+  let updatetodo = await todoSchema.findOneAndUpdate(
+    { _id: id },
+    { task: task },
+    { new: true }
+  );
+  res.status(200).send({ success: "updated successful", data: updatetodo });
+});
 
 app.listen(3000, () => {
-  console.log("server is running")
-})
+  console.log("server is running");
+});
